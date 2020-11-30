1b3dd5cbb761666bd498f45e51a20ec944ac8c7e
LOISEAU Evan <evan.loiseau@epitech.eu>
2020-11-30 16:06:59 +0100

    OpenCV: removing unused features from the code

--- OpenCV/sources/main.cpp
+++ OpenCV/sources/main.cpp
@@ -14,19 +14,11 @@ int main()
 {
     // Create a VideoCapture object and use camera to capture the video
     cv::VideoCapture cap("test.mp4");
-
-    // Check if camera opened successfully
     if (!cap.isOpened()) {
         std::cerr << "Error opening video stream" << std::endl;
         return -1;
     }
 
-    // Default resolution of the frame is obtained.The default resolution is system dependent.
-    int frame_width  = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-    // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
-    cv::VideoWriter video("outcpp.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frame_width, frame_height));
     while (1) {
         cv::Mat frame;
 
@@ -34,11 +26,9 @@ int main()
         cap >> frame;
 
         // If the frame is empty, break immediately
-        if (frame.empty())
+        if (frame.empty()) {
             break;
-
-        // Write the frame into the file 'outcpp.avi'
-        video.write(frame);
+        }
 
         // Display the resulting frame
         imshow("Frame", frame);
@@ -49,12 +39,5 @@ int main()
             break;
         }
     }
-
-    // When everything done, release the video capture and write object
-    cap.release();
-    video.release();
-
-    // Closes all the windows
-    cv::destroyAllWindows();
     return 0;
 }
