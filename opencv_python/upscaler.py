9920cd590076352f9b5da3694ba951192081cd96
Ersikan <julien.philippon@epitech.eu>
2020-12-01 11:38:19 +0100

    Display video and upscaled version side by side

--- opencv_python/upscaler.py
+++ opencv_python/upscaler.py
@@ -2,19 +2,44 @@
 #
 import cv2
 import sys
+import numpy as np
 
-cap = cv2.VideoCapture(sys.argv[1])
+screen = (1920, 1080)
+downscale_ratio = 4
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.namedWindow("frame", cv2.WINDOW_FULLSCREEN)
-    cv2.imshow("frame", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def scale(frame, ratio, *, interpolation=cv2.INTER_AREA):
+    dim = (int(frame.shape[1] * ratio), int(frame.shape[0] * ratio))
+    return cv2.resize(frame, dim, interpolation=interpolation)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(sys.argv[1])
+    window_fit_scale = None
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Fit the frame to half the window
+        if not window_fit_scale:
+            window_fit_scale = screen[0] / 2.0 / frame.shape[1]
+
+        frame = scale(frame, window_fit_scale)
+
+        # downscale then upscale
+        downscaled = scale(frame, 1 / downscale_ratio)
+        upscaled = scale(downscaled, downscale_ratio)
+
+        # Merge images
+        merged = np.concatenate((frame, upscaled), axis=1)
+
+        # Display the resulting frame
+        cv2.imshow("result", merged)
+        # cv2.imshow("downscaled", downscaled)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
